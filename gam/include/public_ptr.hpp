e98761c3b012c1ac89dc425e7520243e02270f85
Paolo Viviani <paolo.vivi@gmail.com>
2018-09-17 17:23:12 +0200

    removed check on public pointer source (self)

--- gam/include/public_ptr.hpp
+++ gam/include/public_ptr.hpp
@@ -246,7 +246,8 @@ public:
      */
     void push(executor_id to) const
     {
-        USRASSERT(to != ctx().rank() && to < ctx().cardinality());
+       // USRASSERT(to != ctx().rank() && to < ctx().cardinality());
+    	USRASSERT(ctx().cardinality());
         if (internal_gp.is_address())
         {
             //pointer brings a global address
